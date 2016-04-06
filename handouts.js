6a6df7c39be6e2ca62a2c14fef71525de2ad0e03
Alleen Tu <alleentu95@gmail.com>
2016-04-06 15:10:07 -0700

    Added files via upload

---
+++ handouts.js
@@ -0,0 +1,544 @@
+$(function() {
+
+  //send a message to the console window to check 
+  //if this page is beting read
+  console.log("ready");
+  
+    //aee  
+    
+  $('#li2').click(function() {
+      
+      
+    resetColors();
+    $('#li2').css('backgroundColor', '#C26E60');
+      
+      resetImage();
+    $('.swfObject1').css('visibility', 'visible');
+      
+  });
+
+    
+  $('#li3').click(function() {
+      
+           resetImage();
+        $('.swfObject2').css('visibility', 'visible');  
+      
+          resetColors();
+       $('#li3').css('backgroundColor', '#C26E60');
+
+  });
+    
+    //anm
+
+  $('#li2-1').click(function() {
+       
+           resetImage();
+       $('.swfObject2-1').css('visibility', 'visible'); 
+      
+         resetColors();
+      $('#li2-1').css('backgroundColor', '#C26E60');
+
+  });
+
+    
+ $('#li2-2').click(function() {
+     
+        resetImage();
+     $('.swfObject2-2').css('visibility', 'visible'); 
+
+     
+               resetColors();
+     $('#li2-2').css('backgroundColor', '#C26E60');
+   
+    
+  });
+
+    
+      $('#li2-3').click(function() {
+          
+           resetImage();
+           $('.swfObject2-3').css('visibility', 'visible'); 
+    
+          
+                    resetColors();
+    $('#li2-3').css('backgroundColor', '#C26E60');
+               
+  });
+    
+    
+    
+      $('#li2-4').click(function() {
+          
+             resetImage();
+     $('.swfObject2-4').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li2-4').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+         $('#li2-5').click(function() {
+          
+             resetImage();
+     $('.swfObject2-5').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li2-5').css('backgroundColor', '#C26E60');
+      
+          
+  });
+ 
+    
+         $('#li3-1').click(function() {
+          
+             resetImage();
+     $('.swfObject3-1').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li3-1').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+         $('#li3-2').click(function() {
+          
+             resetImage();
+     $('.swfObject3-2').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li3-2').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+         $('#li3-3').click(function() {
+          
+             resetImage();
+     $('.swfObject3-3').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li3-3').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+         $('#li3-5').click(function() {
+          
+             resetImage();
+     $('.swfObject3-4').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li3-5').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+         $('#li3-6').click(function() {
+          
+             resetImage();
+     $('.swfObject3-5').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li3-6').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+         $('#li3-7').click(function() {
+          
+             resetImage();
+     $('.swfObject3-6').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li3-7').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+         $('#li3-8').click(function() {
+          
+             resetImage();
+     $('.swfObject3-7').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li3-8').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+         $('#li3-10').click(function() {
+          
+             resetImage();
+     $('.swfObject3-8').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li3-10').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+         $('#li3-11').click(function() {
+          
+             resetImage();
+     $('.swfObject3-9').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li3-11').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+         $('#li3-12').click(function() {
+          
+             resetImage();
+     $('.swfObject3-10').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li3-12').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+         $('#li3-13').click(function() {
+          
+             resetImage();
+     $('.swfObject3-11').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li3-13').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+        $('#li3-14').click(function() {
+          
+             resetImage();
+     $('.swfObject3-12').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li3-14').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+        $('#li4-1').click(function() {
+          
+             resetImage();
+     $('.swfObject4-1').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li4-1').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+        $('#li4-2').click(function() {
+          
+             resetImage();
+     $('.swfObject4-2').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li4-2').css('backgroundColor', '#C26E60');
+      
+          
+  });        
+        $('#li4-3').click(function() {
+          
+             resetImage();
+     $('.swfObject4-3').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li4-3').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+        $('#li5-1').click(function() {
+          
+             resetImage();
+     $('.swfObject5-1').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li5-1').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+        $('#li5-2').click(function() {
+          
+             resetImage();
+     $('.swfObject5-2').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li5-2').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+        $('#li5-3').click(function() {
+          
+             resetImage();
+     $('.swfObject5-3').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li5-3').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+        $('#li5-4').click(function() {
+          
+             resetImage();
+     $('.swfObject5-4').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li5-4').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+        $('#li5-5').click(function() {
+          
+             resetImage();
+     $('.swfObject5-5').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li5-5').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+        $('#li5-6').click(function() {
+          
+             resetImage();
+     $('.swfObject5-6').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li5-6').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+        $('#li5-7').click(function() {
+          
+             resetImage();
+     $('.swfObject5-7').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li5-7').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+        $('#li6-1').click(function() {
+          
+             resetImage();
+     $('.swfObject6-1').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li6-1').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+        $('#li6-2').click(function() {
+          
+             resetImage();
+     $('.swfObject6-2').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li6-2').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+        $('#li6-3').click(function() {
+          
+             resetImage();
+     $('.swfObject6-3').css('visibility', 'visible'); 
+          
+        
+                    resetColors();
+    $('#li6-3').css('backgroundColor', '#C26E60');
+      
+          
+  });
+    
+    $('#aeeButton').click(function() {
+          
+             resetImage();
+     $('#aeeButton').css('backgroundColor', '#C26E60'); 
+          $('#anmButton').css('backgroundColor',
+                               'grey');
+         $('#ansButton').css('backgroundColor',
+                               'grey');
+        $('#miscButton').css('backgroundColor',
+                               'grey');
+        $('#preprofButton').css('backgroundColor',
+                               'grey');
+        $('#prevetButton').css('backgroundColor',
+                               'grey');
+                    resetColors();
+        $('#aeeUl').css('visibility', 'visible');
+        $('#anmUl').css('visibility','hidden');
+        $('#ansUl').css('visibility','hidden'); 
+        $('#miscUl').css('visibility','hidden');
+        $('#preprofUl').css('visibility','hidden');
+        $('#prevetUl').css('visibility','hidden');
+  });
+    
+    $('#anmButton').click(function() {
+          
+             resetImage();
+     $('#anmButton').css('backgroundColor', '#C26E60'); 
+          $('#aeeButton').css('backgroundColor',
+                               'grey');
+         $('#ansButton').css('backgroundColor',
+                               'grey');
+        $('#miscButton').css('backgroundColor',
+                               'grey');
+        $('#preprofButton').css('backgroundColor',
+                               'grey');
+        $('#prevetButton').css('backgroundColor',
+                               'grey');
+                    resetColors();
+        $('#anmUl').css('visibility', 'visible');
+        $('#aeeUl').css('visibility','hidden');
+        $('#ansUl').css('visibility','hidden'); 
+        $('#miscUl').css('visibility','hidden');
+        $('#preprofUl').css('visibility','hidden');
+        $('#prevetUl').css('visibility','hidden');
+  });
+    
+    $('#ansButton').click(function() {
+          
+             resetImage();
+     $('#ansButton').css('backgroundColor', '#C26E60'); 
+          $('#anmButton').css('backgroundColor',
+                               'grey');
+         $('#aeeButton').css('backgroundColor',
+                               'grey');
+        $('#miscButton').css('backgroundColor',
+                               'grey');
+        $('#preprofButton').css('backgroundColor',
+                               'grey');
+        $('#prevetButton').css('backgroundColor',
+                               'grey');
+                    resetColors();
+        $('#ansUl').css('visibility', 'visible');
+        $('#anmUl').css('visibility','hidden');
+        $('#aeeUl').css('visibility','hidden'); 
+        $('#miscUl').css('visibility','hidden');
+        $('#preprofUl').css('visibility','hidden');
+        $('#prevetUl').css('visibility','hidden');
+  });
+    
+    $('#miscButton').click(function() {
+          
+             resetImage();
+     $('#miscButton').css('backgroundColor', '#C26E60'); 
+          $('#anmButton').css('backgroundColor',
+                               'grey');
+         $('#ansButton').css('backgroundColor',
+                               'grey');
+        $('#aeeButton').css('backgroundColor',
+                               'grey');
+        $('#preprofButton').css('backgroundColor',
+                               'grey');
+        $('#prevetButton').css('backgroundColor',
+                               'grey');
+                    resetColors();
+        $('#miscUl').css('visibility', 'visible');
+        $('#anmUl').css('visibility','hidden');
+        $('#ansUl').css('visibility','hidden'); 
+        $('#aeeUl').css('visibility','hidden');
+        $('#preprofUl').css('visibility','hidden');
+        $('#prevetUl').css('visibility','hidden');
+  });
+    
+    $('#preprofButton').click(function() {
+          
+             resetImage();
+     $('#preprofButton').css('backgroundColor', '#C26E60'); 
+          $('#anmButton').css('backgroundColor',
+                               'grey');
+         $('#ansButton').css('backgroundColor',
+                               'grey');
+        $('#miscButton').css('backgroundColor',
+                               'grey');
+        $('#aeeButton').css('backgroundColor',
+                               'grey');
+        $('#prevetButton').css('backgroundColor',
+                               'grey');
+                    resetColors();
+        $('#preprofUl').css('visibility', 'visible');
+        $('#anmUl').css('visibility','hidden');
+        $('#ansUl').css('visibility','hidden'); 
+        $('#miscUl').css('visibility','hidden');
+        $('#aeeUl').css('visibility','hidden');
+        $('#prevetUl').css('visibility','hidden');
+  });
+    
+    $('#prevetButton').click(function() {
+          
+             resetImage();
+     $('#prevetButton').css('backgroundColor', '#C26E60'); 
+          $('#anmButton').css('backgroundColor',
+                               'grey');
+         $('#ansButton').css('backgroundColor',
+                               'grey');
+        $('#miscButton').css('backgroundColor',
+                               'grey');
+        $('#preprofButton').css('backgroundColor',
+                               'grey');
+        $('#aeeButton').css('backgroundColor',
+                               'grey');
+                    resetColors();
+        $('#prevetUl').css('visibility', 'visible');
+        $('#anmUl').css('visibility','hidden');
+        $('#ansUl').css('visibility','hidden'); 
+        $('#miscUl').css('visibility','hidden');
+        $('#preprofUl').css('visibility','hidden');
+        $('#aeeUl').css('visibility','hidden');
+  });
+
+    
+    function resetColors(){
+        console.log("resetting colors");
+        $('#column2 li').css('backgroundColor', 'white');
+        
+    }
+    
+    function resetImage(){
+     console.log("resetting image");
+        $('#column4 embed').css('visibility', 'hidden');
+    }
+  //close jQuery
+});
